cfe13447ee8ffe6b444b7d9e10e17f5229523e59
krisztianavi <avikrisz@gmail.com>
2025-02-17 13:29:50 +0100

    backend updated with blob

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,34 @@
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient()
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
 async function main() {
+  for (let i = 0; i < 10; i++) {
+    const mp3Buffer = Buffer.from(faker.string.alphanumeric(100), 'utf-8');
+    const imageUrl = faker.image.url();
+    const imageBuffer = Buffer.from(imageUrl, 'utf-8');
+
+    await prisma.zeneszamok.create({
+      data: {
+        artist: faker.person.fullName(),
+        album: faker.music.album(),
+        cim: faker.music.songName(),
+        hossz: faker.date.past(),
+        kiadas_ev: faker.number.int({ min: 1980, max: 2025 }),
+        mufaj: faker.music.genre(),
+        mp3: mp3Buffer,  
+        boritokep: imageBuffer
+      } as any 
+    });
+  }
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
   })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)})
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ZeneszamokModule } from './zeneszamok/zeneszamok.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ZeneszamokModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
