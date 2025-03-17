f5ba0628afed889ade9b47aaaf5de20457e2192e
laszlokdorii <laszlok.dora@gmail.com>
2025-03-17 12:00:37 +0100

    navbar fix

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,39 +1,95 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Brand from '../../../public/genius-removebg-preview.png'
-import './navbar.css'
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { TiHomeOutline } from "react-icons/ti";
+import { MdOutlineLogout } from "react-icons/md";
+import Brand from "../../../public/genius-removebg-preview.png";
+import "./navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
 
   const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar)
-  }
+    setShowNavbar(!showNavbar);
+    if (!showNavbar) setShowSearch(false);
+  };
+
+  const handleShowSearch = () => {
+    setShowSearch(!showSearch);
+    if (!showSearch) setShowNavbar(false);
+  };
 
   return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="menu-icon" onClick={handleShowNavbar}>
-          <GiHamburgerMenu />
+    <nav className="navbar navbar-expand-md navbar-light fixed-top">
+      <div className="container-fluid d-flex align-items-center justify-content-between">
+
+        <div className="navbar-left">
+          <button className="navbar-toggler" type="button" onClick={handleShowNavbar}>
+            <GiHamburgerMenu className="menu-icon" />
+          </button>
+          <img src={Brand} className="brl" alt="brandlogo" />
         </div>
 
-        <div className="logo">
-          <img src={Brand} className='brl' alt="brandlogo" />
+        <div className="navbar-center">
+          <div className="search-bar-container d-none d-md-flex">
+            <div className="search-bar">
+              <FaSearch color="white" className="search-icon" />
+              <input type="text" className="form-control" placeholder="Search..." />
+            </div>
+          </div>
         </div>
 
-        <div className={`nav-elements  ${showNavbar && 'active'}`}>
-          <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/blog">Blog</NavLink></li>
-            <li><NavLink to="/projects">Projects</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-          </ul>
+        <div className="navbar-right">
+          <button className="search-icon-btn d-md-none" onClick={handleShowSearch}>
+            <FaSearch />
+          </button>
         </div>
       </div>
-    </nav>
-  )
-}
 
-export default Navbar
+      {showSearch && (
+        <div className="search-bar-container show-search">
+          <div className="search-bar">
+            <FaSearch color="white" className="search-icon" />
+            <input type="text" className="form-control" placeholder="Search..." />
+          </div>
+        </div>
+      )}
+
+      <div className="desktop-menu d-none d-md-flex">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/"><TiHomeOutline /></NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/profile"><CgProfile /></NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/logout"><MdOutlineLogout /></NavLink>
+          </li>
+        </ul>
+      </div>
+
+      {showNavbar && (
+        <div className="mobile-menu">
+          <ul className="mobile-menu-list">
+            <li>
+              <NavLink className="nav-link" to="/" onClick={handleShowNavbar}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/profile" onClick={handleShowNavbar}>Profile</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/logout" onClick={handleShowNavbar}>Log out</NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
